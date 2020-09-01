b7319f9b5e49672e1bae4af3dba8d5cbb2dde4a5
Pat O'Callaghan <pat@intercom.io>
2020-09-01 22:26:35 +0100

    Init commit

---
+++ bar-website/index.js
@@ -0,0 +1,25 @@
+class Bar {
+  constructor() {
+    this.barEndpoint = "http://localhost:3000/bar";
+
+    $(document).ready(() => {
+      this.verify();
+    });
+  }
+
+  async verify() {
+    try {
+      let response = await $.post(this.barEndpoint).then();
+      if (response.success === true) {
+        alert("Yay! Everything works");
+      }
+      else {
+        alert("Unexpected response");
+      }
+    }
+    catch (error) {
+      alert("There has been a problem with the request");
+    }
+  }
+
+}
